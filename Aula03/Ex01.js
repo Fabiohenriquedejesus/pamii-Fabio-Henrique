7bde21867317593142412f944579ac24ab8eefe0
Fabio Henrique <Fabiohjr15@gmail.com>
2025-03-25 17:52:57 -0300

    Aula03

---
+++ Aula03/Ex01.js
@@ -0,0 +1,17 @@
+const n = prompt("Digite um numero");
+
+const total=n/2;
+
+if(n/2==2){
+
+   window.alert("Par");
+
+}
+
+if(n/2==1){
+
+window.alert("Impar");
+
+}
+
+window.alert(total);
---
+++ Aula03/Ex02.js
@@ -0,0 +1,19 @@
+const n1 = prompt("Digite um numero");
+
+if (n1 > 0){
+
+window.alert("positivo");
+
+}
+
+if (n2==0){
+
+window.alert("zero");
+
+}
+
+if (n3 < 0){
+
+window.Text("negativo");
+
+}
